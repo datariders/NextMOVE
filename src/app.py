6fe1dd2330a51c44e20ccbbbe0452b8c2c5742a6
dataridershub <dataridershub@gmail.com>
2024-06-23 21:34:40 -0700

    Add files via upload

--- src/app.py
+++ src/app.py
@@ -5,12 +5,10 @@ import streamlit as st
 
 
 def main():
-    uri = get_mongodb_cluster_connection_uri(MONGODB_USERNAME, MONGODB_USER_PASSWORD, MONGODB_CLUSTER_HOSTNAME, MONGODB_CLUSTERNAME)
-    if uri is not None:
-        #print(" uri: ", uri, "\t type(uri): ", type(uri))
-
+    uri = get_mongodb_cluster_connection_uri(MONGODB_USERNAME, MONGODB_USER_PASSWORD, MONGODB_CLUSTER_HOSTNAME)
+    if uri:
         mongodb_client = get_mongodb_cluster_client(uri)
-        if mongodb_client is not None:
+        if mongodb_client:
             games_db = get_mongodb_database(mongodb_client, "vectorsdb")
             #print(" games_db: ", games_db, "\t type(games_db): ", type(games_db))
 
@@ -21,19 +19,21 @@ def main():
                 chat_history_collection = get_collection(games_db, "chat_history")
                 #print(" chat_history_collection: ", chat_history_collection, "\t type(chat_history_collection): ", type(chat_history_collection))
 
+
     # Streamlit interface
     st.image('assets/header.png')
     st.title("NextMOVE: Chess training assistant")
 
     # Upload PDF
     uploaded_file = st.file_uploader("Upload Chess games (pdf)", type="pdf")
-    if uploaded_file is not None:
+    if uploaded_file:
+        #print(" uploaded_file: ", uploaded_file, "\t type(uploaded_file): ", type(uploaded_file))
         text = get_text_from_pdf(uploaded_file)
-        if text is not None:
+        if text:
             # Vectorize text
             embeddings = vectorize_text(text)
             if embeddings is not None:
-                print(" embeddings: ", embeddings, "\t type(embeddings): ", type(embeddings))
+                #print(" embeddings: ", embeddings, "\t type(embeddings): ", type(embeddings))
 
                 # Save vector to MongoDB
                 save_embeddings_to_collection(embeddings, text, games_collection)
@@ -41,17 +41,17 @@ def main():
                 st.success("Game embeddings are saved to MongoDB collection successfully!")
 
                 user_query = st.text_input("Enter your move:")
-                if user_query is not None and games_collection is not None and chat_history_collection is not None:
+                if user_query and games_collection is not None and chat_history_collection is not None:
                     print(" user_query: ", user_query, "\t type(user_query): ", type(user_query))
                     
                     # Retrieve relevant documents
                     relevant_docs = retrieve_relevant_docs(user_query, games_collection)
-                    if relevant_docs is not None:
+                    if relevant_docs:
                         print(" relevant_docs: ", relevant_docs, "\t type(relevant_docs): ", type(relevant_docs))
 
                         # Generate response
                         bot_response = generate_response(user_query, relevant_docs)
-                        if bot_response is not None:
+                        if bot_response:
                             print(" bot_response: ", bot_response, "\t type(bot_response): ", type(bot_response))
 
                             # Display response
--- src/util.py
+++ src/util.py
@@ -6,13 +6,11 @@ import pymupdf
 from sentence_transformers import SentenceTransformer
 import openai
 
-
-
 # Initialize OpenAI API (Replace with your API key)
 openai.api_key = OPENAI_API_KEY
 
 
-def get_mongodb_cluster_connection_uri(mongodb_username: str, mongodb_user_password: str, mongodb_cluster_hostname: str, mongodb_cluster_name: str) -> str:
+def get_mongodb_cluster_connection_uri(mongodb_username: str, mongodb_user_password: str, mongodb_cluster_hostname: str) -> str: 
     """
     Return the MongoDB cluster connection URI
 
@@ -20,20 +18,24 @@ def get_mongodb_cluster_connection_uri(mongodb_username: str, mongodb_user_passw
     mongodb_username (str): MongoDB username
     mongodb_user_password (str): Password of the MongoDB user
     mongodb_cluster_hostname (str): MongoDB cluster hostname
-    mongodb_cluster_name (str): MongoDB cluster name
 
     Returns:
     str: MongoDB cluster connection uri
     """
 
-    return ("mongodb+srv://" +
-            mongodb_username +
-            ":" +
-            mongodb_user_password +
-            "@" +
-            mongodb_cluster_hostname +
-            "/?retryWrites=true&w=majority&appName=" +
-            mongodb_cluster_name)
+    mongodb_cluster_hostname_str = mongodb_cluster_hostname.split(".") 
+    mongodb_clustername = mongodb_cluster_hostname_str[0]
+    
+    uri = ("mongodb+srv://" +
+           mongodb_username +
+           ":" +
+           mongodb_user_password +
+           "@" +
+           mongodb_cluster_hostname +
+           "/?retryWrites=true&w=majority&appName=" +
+           mongodb_clustername)
+    #print(" uri: ", uri, "\t type(uri): ", type(uri))
+    return uri
 
 
 def get_mongodb_cluster_client(uri: str) -> MongoClient:
@@ -48,7 +50,7 @@ def get_mongodb_cluster_client(uri: str) -> MongoClient:
     """
 
     client = None
-    if uri is not None:
+    if uri:
         try:
             # Create a new client and connect to the server
             client = MongoClient(uri, server_api=ServerApi('1'))
@@ -56,10 +58,11 @@ def get_mongodb_cluster_client(uri: str) -> MongoClient:
 
             # Send a ping to confirm a successful connection
             client.admin.command('ping')
-            print("Pinged your deployment. You successfully connected to MongoDB!")
+            print("Pinged the MongoDB cluster deployment. Successfully connected to MongoDB cluster!")
         except Exception as e:
             print(f"{e}")
-            raise Exception('Failed to connect to MongoDB database.  Please supply valid MONGODB_USERNAME, MONGODB_USER_PASSWORD, MONGODB_CLUSTER_HOSTNAME, MONGODB_CLUSTERNAME parameters') from e
+            raise Exception(
+                'Failed to connect to MongoDB database.  Please supply valid MongoDB username, MongoDB user password, MongoDB cluster hostname parameters') from e
 
     return client
 
@@ -67,7 +70,7 @@ def get_mongodb_cluster_client(uri: str) -> MongoClient:
 def get_mongodb_database(mongodb_client, database_name: str):
     """ Returns vectorsdb """
     db = None
-    if mongodb_client is not None and database_name is not None: 
+    if mongodb_client and database_name:
         db = mongodb_client[database_name]
         print(" db: ", db, "\t type(db): ", type(db))
     return db
@@ -75,7 +78,7 @@ def get_mongodb_database(mongodb_client, database_name: str):
 
 def get_collection(db, collection_name: str):
     collection = None
-    if db is not None and collection_name is not None:
+    if db is not None and collection_name:
         collection = db[collection_name]
         print(" collection: ", collection, "\t type(collection): ", type(collection))
     return collection
@@ -84,7 +87,7 @@ def get_collection(db, collection_name: str):
 def extract_text_from_pdf(pdf_path):
     text = ""
 
-    if pdf_path is not None:
+    if pdf_path:
         try:
             doc = pymupdf.open(pdf_path)  # Open the PDF document
             if doc is not None:
@@ -105,16 +108,17 @@ def extract_text_from_pdf(pdf_path):
 def get_sentence_transformer_model():
     return SentenceTransformer('paraphrase-MiniLM-L6-v2')
     #return SentenceTransformer('sentence-transformers/paraphrase-MiniLM-L6-v2')
-"""    
+"""
+
 
 # Function to vectorize text
 def vectorize_text(text):
     embeddings = None
 
-    if text is not None:
+    if text:
         try:
             model = SentenceTransformer("all-MiniLM-L6-v2")
-            if model is not None:
+            if model:
                 embeddings = model.encode(text)
                 print(" embeddings: ", embeddings, "\t type(embeddings): ", type(embeddings))
         except Exception as e:
@@ -130,44 +134,46 @@ def save_embeddings_to_collection(embeddings, text, collection):
     print("\n text: ", text)
     print("\n collection: ", collection)
 
-    if embeddings is not None and text is not None and collection is not None:
+    if embeddings is not None and text and collection is not None:
         document = {
             'text': text,
             'vector': embeddings.tolist()  # Convert numpy array to list
         }
         collection.insert_one(document)
-        
+
 
 # Function to retrieve relevant documents from MongoDB
 def retrieve_relevant_docs(query, collection):
-    if query is not None and collection is not None:
-        #model = get_sentence_transformer_model()
+    if query and collection is not None:
+        # model = get_sentence_transformer_model()
         model = SentenceTransformer("all-MiniLM-L6-v2")
-        if model is not None:
+        if model:
             query_vector = model.encode(query).tolist()
             docs = list(collection.find())
-            if docs is not None and query_vector is not None:
-                relevant_docs = sorted(docs, key=lambda doc: cosine_similarity(query_vector, doc['vector']), reverse=True)[:5]
+            if docs and query_vector:
+                relevant_docs = sorted(docs, key=lambda doc: cosine_similarity(query_vector, doc['vector']),
+                                       reverse=True)[:5]
                 return relevant_docs
 
 
 # Cosine similarity function
 def cosine_similarity(vec1, vec2):
-    if vec1 is not None and vec2 is not None:
+    if vec1 and vec2:
         print(" vec1: ", vec1, "\t type(vec1): ", type(vec1))
         print(" vec2: ", vec2, "\t type(vec2): ", type(vec2))
 
-        result = sum(a * b for a, b in zip(vec1, vec2)) / (sum(a * a for a in vec1) ** 0.5 * sum(b * b for b in vec2) ** 0.5)
+        result = sum(a * b for a, b in zip(vec1, vec2)) / (
+                    sum(a * a for a in vec1) ** 0.5 * sum(b * b for b in vec2) ** 0.5)
         print(" result: ", result, "\t type(result): ", type(result))
         return result
-        #return sum(a * b for a, b in zip(vec1, vec2)) / (sum(a * a for a in vec1) ** 0.5 * sum(b * b for b in vec2) ** 0.5)
+        # return sum(a * b for a, b in zip(vec1, vec2)) / (sum(a * a for a in vec1) ** 0.5 * sum(b * b for b in vec2) ** 0.5)
 
 
 # Function to generate chatbot response using OpenAI GPT
 def generate_response(query, relevant_docs):
-    if query is not None and relevant_docs is not None:
+    if query and relevant_docs:
         augmented_query = query + " " + " ".join([doc['text'] for doc in relevant_docs])
-        if augmented_query is not None:
+        if augmented_query:
             print(" augmented_query: ", augmented_query, "\t type(augmented_query): ", type(augmented_query))
             response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
@@ -177,14 +183,14 @@ def generate_response(query, relevant_docs):
                 ],
                 max_tokens=150
             )
-            if response is not None:
+            if response:
                 print(" response: ", response, "\t type(response): ", type(response))
                 return response['choices'][0]['message']['content'].strip()
 
 
 # Function to save chat history to MongoDB
 def save_chat_history(user_query, nextmove_response, collection):
-    if user_query is not None and nextmove_response is not None and collection is not None:
+    if user_query and nextmove_response and collection is not None:
         document = {
             'user_query': user_query,
             'nextmove_response': nextmove_response
@@ -193,7 +199,7 @@ def save_chat_history(user_query, nextmove_response, collection):
 
 
 def get_text_from_pdf(file):
-    if file is not None:
+    if file:
         # Save the uploaded PDF to a temporary file
         with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
             tmp_file.write(file.read())
@@ -205,3 +211,9 @@ def get_text_from_pdf(file):
 
         return text
     return None
+
+
+if __name__ == "__main__":
+    s = "mongodbcluster0.bjmkbwc.mongodb.net"
+    result = s.split(".")
+    print(" result[0]: ", result[0])
